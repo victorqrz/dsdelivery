890f0d63424f108b259539a507c1c700666125a5
Victor <victoorqrz@gmail.com>
2021-01-09 09:37:00 -0300

    POST /orders

--- backend/src/main/java/com/devsuperior/dsdelivery/controllers/OrderController.java
+++ backend/src/main/java/com/devsuperior/dsdelivery/controllers/OrderController.java
@@ -5,11 +5,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dsdelivery.dto.OrderDTO;
 import com.devsuperior.dsdelivery.services.OrderService;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/orders")
@@ -24,4 +29,11 @@ public class OrderController {
 		return ResponseEntity.ok().body(list);
 	}
 	
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
 }
